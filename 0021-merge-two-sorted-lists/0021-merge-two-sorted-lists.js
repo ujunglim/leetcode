a66d0279348e4c33e9ffa06e6422d866948e13e8
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-11-24 00:11:08 +0800

    Time: 75 ms (89.90%), Space: 43.7 MB (94.89%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,15 +11,36 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(!list1) return list2;
-    if(!list2) return list1;
+    let dummy = new ListNode(null);
+    let curr = dummy;
     
-    if(list1.val <= list2.val) {
-        list1.next = mergeTwoLists(list1.next, list2);
-        return list1;
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            curr.next = list1;
+            curr = list1;
+            list1 = list1.next;
+        }
+        else {
+            curr.next = list2;
+            curr = list2;
+            list2 = list2.next;
+        }
     }
-    else {
-        list2.next = mergeTwoLists(list1, list2.next);
-        return list2;
-    }
+    
+    curr.next = list1 ? list1 : list2;
+    return dummy.next;
 };
+// ==================================================
+// var mergeTwoLists = function(list1, list2) {
+//     if(!list1) return list2;
+//     if(!list2) return list1;
+    
+//     if(list1.val <= list2.val) {
+//         list1.next = mergeTwoLists(list1.next, list2);
+//         return list1;
+//     }
+//     else {
+//         list2.next = mergeTwoLists(list1, list2.next);
+//         return list2;
+//     }
+// };
