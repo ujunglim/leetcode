b8cf92c162ee30d65d9b21804680389ff74bf4ba
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-11-25 00:28:10 +0800

    Time: 181 ms (39.17%), Space: 48.1 MB (50.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,29 +2,55 @@
  * @param {string} s
  * @return {number}
  */
-// compare value by two pointers i, j.
-// If the values are same, then record the max length of substring into maxs[]
-// 0 <= s.length
-var lengthOfLongestSubstring = function (s) {
-  if (s.length === 0) return 0;
-  if (s.length === 1) return 1;
 
-  let str = s[0]; // 
-  let maxs = [];
+// without reapeat (hash)
+// continue, series (queue)
+var lengthOfLongestSubstring = function (s) {
+  let queue = [];
+  const map = {};
+  let max = 0;
 
   for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j < s.length; j++) {
-      // repeated
-      if (!s.slice(i, j).includes(s[j])) {
-        str = s.slice(i, j + 1);
-      } else {
-        maxs.push(str.length);
-        str = "";
-        break;
+    const curr = s[i];
+    // no repeat
+    if (!map[curr]) {
+      map[curr] = true;
+      queue.push(curr);
+      max = Math.max(max, queue.length); // max++
+    } else {
+      // when repeat, dequeue elements from 0 to repeated element
+      const prevIdx = queue.indexOf(curr);
+      // queue.splice(prevIdx + 1);
+      for (let i = 0; i <= prevIdx; i++) {
+        queue.shift();
       }
+      queue.push(curr);
+      max = Math.max(max, queue.length);
     }
-    if (str) maxs.push(str.length);
   }
-  console.log(Math.max(...maxs));
-  return Math.max(...maxs);
+  return max;
 };
+// ============================================
+// var lengthOfLongestSubstring = function (s) {
+//   if (s.length === 0) return 0;
+//   if (s.length === 1) return 1;
+
+//   let str = s[0]; // 
+//   let maxs = [];
+
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i + 1; j < s.length; j++) {
+//       // repeated
+//       if (!s.slice(i, j).includes(s[j])) {
+//         str = s.slice(i, j + 1);
+//       } else {
+//         maxs.push(str.length);
+//         str = "";
+//         break;
+//       }
+//     }
+//     if (str) maxs.push(str.length);
+//   }
+//   console.log(Math.max(...maxs));
+//   return Math.max(...maxs);
+// };
