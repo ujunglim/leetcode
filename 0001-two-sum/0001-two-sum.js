e43ee7ae0ed8b75ac37b42508b572bb895ad1655
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-11-25 14:32:50 +0800

    Time: 78 ms (92.51%), Space: 42.9 MB (35.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,27 @@
  * @param {number} target
  * @return {number[]}
  */
-
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const secondNum = target - nums[i];
+
+    if (map.has(secondNum)) {
+      return [i, map.get(secondNum)];
+    } else {
+      map.set(nums[i], i);
     }
   }
   return null;
 };
+
+// =========================================
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j];
+//     }
+//   }
+//   return null;
+// };
