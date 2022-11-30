612895bc7799e0355cf6ebdafd82ba2d9a26b35b
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-12-01 00:42:48 +0800

    Time: 144 ms, Memory: 50.6 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set(nums);
+    
+    return nums.length !== set.size ? true : false;
+};
