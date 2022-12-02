65c1737e9f473a7c13d5047e40840ba0d6d01d71
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-12-02 16:13:45 +0800

    Time: 65 ms, Memory: 41.9 MB - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+        if(haystack.includes(needle)) {
+            return haystack.indexOf(needle);
+        }
+        return -1
+    };
