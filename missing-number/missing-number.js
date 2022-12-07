0789cdbd4cf3d12aedb857bf77d165bba04ac7f1
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-12-07 20:41:28 +0800

    Time: 97 ms, Memory: 43.7 MB - LeetHub

--- missing-number/missing-number.js
+++ missing-number/missing-number.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-  const set = new Set(nums);
-  for (let num = 0; num <= nums.length; num++) {
-    if (!set.has(num)) {
-      return num;
-    }
-  }
+    const expectedSum = (nums.length*(nums.length + 1)) / 2;
+    const sum = nums.reduce((acc, prev) => acc + prev);
+    return expectedSum - sum;
 };
+// var missingNumber = function (nums) {
+//   const set = new Set(nums);
+//   for (let num = 0; num <= nums.length; num++) {
+//     if (!set.has(num)) {
+//       return num;
+//     }
+//   }
+// };
 // var missingNumber = function (nums) {
 //   for (let i = 0; i <= nums.length; i++) {
 //     if (!nums.includes(i)) return i;
