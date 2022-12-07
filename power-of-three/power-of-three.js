31847be165aa6e4c25a045706b7fbba3071a3b58
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-12-07 17:30:59 +0800

    Time: 405 ms, Memory: 50.6 MB - LeetHub

--- power-of-three/power-of-three.js
+++ power-of-three/power-of-three.js
@@ -2,14 +2,23 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = function (n) {
-  if (n === 1) return true;
-  if (n <= 0) return false;
-  // when time <= 1
-  if (Math.abs(n / 3) <= 1) {
-    if (n % 3 === 0) return true;
-    return false;
-  } else {
-    return isPowerOfThree(n / 3);
-  }
+var isPowerOfThree = function(n) {
+    if(n < 1) return false;
+    
+    while(n % 3 === 0) {
+        n /= 3;
+    }
+    return n === 1;
 };
+// // case: -3, 1
+// var isPowerOfThree = function (n) {
+//   if (n === 1) return true;
+//   if (n <= 0) return false;
+//   // when time <= 1
+//   if (Math.abs(n / 3) <= 1) {
+//     if (n % 3 === 0) return true;
+//     return false;
+//   } else {
+//     return isPowerOfThree(n / 3);
+//   }
+// };
