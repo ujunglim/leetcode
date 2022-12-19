bd762c69f47490b1eddd42129411632d909920c2
ujunglim <37919853+ujunglim@users.noreply.github.com>
2022-12-19 19:05:36 +0800

    Time: 101 ms (29.68%), Space: 42.7 MB (11.54%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
+
+  // <= 찾을때는
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return nums[mid] > target ? mid : mid + 1;
+};
