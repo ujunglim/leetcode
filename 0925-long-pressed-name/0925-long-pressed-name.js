9191ee4badd5086c1c03b26ba5dd0778f4799adf
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-10-26 14:26:45 +0900

    Time: 114 ms (5.49%), Space: 50 MB (5.49%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+
+function getRepeatedCountArr(str) {
+    const result = [];
+    let i = 0;
+    
+    while (i < str.length) {
+        let j = i;
+        while(str[i] === str[j]) {
+            j++;
+        }
+        result.push({
+            char: str[i],
+            count: j-i,
+        });
+        i = j;
+    }
+    return result;
+}
+
+var isLongPressedName = function(name, typed) {
+    // name, typed 각각 char의 중복 횟수를 구한다.
+    const nameArr = getRepeatedCountArr(name);
+    const typeArr = getRepeatedCountArr(typed);
+    console.log(nameArr, typeArr)
+    
+    // name, typed의 고유문자 갯수가 다르면 false
+    if (nameArr.length !== typeArr.length) {
+        return false;
+    }
+    
+    for (let i = 0 ; i < nameArr.length; i++) {
+        // name중복 횟수가 typed중복 횟수 크거나 문자가 다르면 false
+        if (nameArr[i].count > typeArr[i].count || nameArr[i].char !== typeArr[i].char) {
+            return false;
+        }
+    }
+    return true;
+};
+
+
+// leelee, lleelee
