b43013f0af11f7157209b6fbd8e01153af775e44
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-10-31 00:04:57 +0900

    Time: 162 ms (38.22%), Space: 59.5 MB (30.61%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -15,25 +15,23 @@ var threeSum = function(nums) {
         if (i > 0 && nums[i-1] === nums[i]) {
             continue; // 첫번째가 이전과 같은 숫자면 스킵한다
         }
-        // 두번째는 첫번째 바로 뒤부터 시작한다.
-        for (let j = i+1; j < nums.length-1; j+=1) {
-            // 세번째
-            let k = nums.length-1;
-            while(j < k) { // 두번쨰와 세번째는 교차할 수 없다 (두번째 < 세번째)
-                const sum = nums[i]+ nums[j]+ nums[k];
-                // 세 숫자의 합이 target이면 result에 저장한다
-                if (sum === target) {
-                    result.push([nums[i], nums[j], nums[k]])
-                    while(nums[k-1] === nums[k]) k--; // 중복되는 세번째는 스킵한다
-                    while(nums[j] === nums[j+1]) j++; // 중복되는 두번째는 스킵한다
-                    // 이제서야 진정으로 두번째와 세번째를 이동한다
-                    k--; 
-                    j++;
-                } else if (sum > target) { // 세 숫자의 합이 target보다 크면 세번째 숫자를 줄인다
-                    k--;
-                } else if (sum < target) { // 세 숫자의 합이 target보다 작으면 두번째 숫자를 늘린다
-                    j++;
-                }
+        j = i+1; // 두번째는 첫번째 바로 뒤부터 시작한다.
+        // 세번째
+        let k = nums.length-1;
+        while(j < k) { // 두번쨰와 세번째는 교차할 수 없다 (두번째 < 세번째)
+            const sum = nums[i]+ nums[j]+ nums[k];
+            // 세 숫자의 합이 target이면 result에 저장한다
+            if (sum === target) {
+                result.push([nums[i], nums[j], nums[k]])
+                while(nums[k-1] === nums[k]) k--; // 중복되는 세번째는 스킵한다
+                while(nums[j] === nums[j+1]) j++; // 중복되는 두번째는 스킵한다
+                // 이제서야 진정으로 두번째와 세번째를 이동한다
+                k--; 
+                j++;
+            } else if (sum > target) { // 세 숫자의 합이 target보다 크면 세번째 숫자를 줄인다
+                k--;
+            } else if (sum < target) { // 세 숫자의 합이 target보다 작으면 두번째 숫자를 늘린다
+                j++;
             }
         }
     }
