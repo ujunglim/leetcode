79e8d1116e08ae549a88b2c434e1e32bbc783316
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-10-31 00:14:47 +0900

    Time: 154 ms (55.62%), Space: 58.8 MB (69.75%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,79 +1,39 @@
-function threeSum(nums) {
-  const results = [];
-
-  // obviously irrelevant if we don't have at least 3 numbers to play with!
-  if (nums.length < 3) return results;
-
-  // having the numbers in ascending order will make this problem much easier.
-  // also, knowing the overall problem  will take at least O(N^2) time, we can
-  // afford the O(NlogN) sort operation
-  nums = nums.sort((a, b) => a - b);
-
-  // if the question asks us for a custom target, we can control it here
-  let target = 0;
-
-  for (let i = 0; i < nums.length - 2; i++) {
-    // `i` represents the "left" most number in our sorted set.
-    // once this number hits 0, there's no need to go further since
-    // positive numbers cannot sum to a negative number
-    if (nums[i] > target) break;
-
-    // we don't want repeats, so skip numbers we've already seen
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-
-    // `j` represents the "middle" element between `i` and `k`.
-    // we will increment this up through the array while `i` and `k`
-    // are anchored to their positions. we will decrement `k` for
-    // for each pass through the array, and finally increment `i`
-    // once `j` and `k` meet.
-    let j = i + 1;
-
-    // `k` represents the "right" most element
-    let k = nums.length - 1;
-
-    // to summarize our setup, we have `i` that starts at the beginning,
-    // `k` that starts at the end, and `j` that races in between the two.
-    //
-    // note that `i` is controlled by our outer for-loop and will move the slowest.
-    // in the meantime, `j` and `k` will take turns inching towards each other depending
-    // on some logic we'll set up below. once they collide, `i` will be incremented up
-    // and we'll repeat the process.
-
-    while (j < k) {
-      let sum = nums[i] + nums[j] + nums[k];
-
-      // if we find the target sum, increment `j` and decrement `k` for
-      // other potential combos where `i` is the anchor
-      if (sum === target) {
-        // store the valid threesum
-        results.push([nums[i], nums[j], nums[k]]);
-
-        // this is important! we need to continue to increment `j` and decrement `k`
-        // as long as those values are duplicated. in other words, we wanna skip values
-        // we've already seen. otherwise, an input array of [-2,0,0,2,2] would result in
-        // [[-2,0,2], [-2,0,2]].
-        //
-        // (i'm not a fan of this part because we're doing a while loop as we're
-        // already inside of another while loop...)
-        while (nums[j] === nums[j + 1]) j++;
-        while (nums[k] === nums[k - 1]) k--;
-
-        // finally, we need to actually move `j` forward and `k` backward to the
-        // next unique elements. the previous while loops will not handle this.
-        j++;
-        k--;
-
-        // if the sum is too small, increment `j` to get closer to the target
-      } else if (sum < target) {
-        j++;
-
-        // if the sum is too large, decrement `k` to get closer to the target
-      } else {
-        // (sum > target)
-        k--;
-      }
+var threeSum = function(nums) {
+    const result = [];
+    const target = 0;
+    if (nums.length < 3) {
+        return result;
     }
-  }
-
-  return results;
-}
+    nums = nums.sort((a, b) => a - b); // 정렬해주어서 포인터의 이동시 판단을 더 간편하게 할 수 있다.
+   
+    // 첫번째 
+    let i = 0;
+    for (let i = 0; i < nums.length-2; i+=1) {
+        if (nums[i] > target) {
+            return result;
+        }
+        if (i > 0 && nums[i-1] === nums[i]) {
+            continue; // 첫번째가 이전과 같은 숫자면 스킵한다
+        }
+        j = i+1; // 두번째는 첫번째 바로 뒤부터 시작한다.
+        // 세번째
+        let k = nums.length-1;
+        while(j < k) { // 두번쨰와 세번째는 교차할 수 없다 (두번째 < 세번째)
+            const sum = nums[i]+ nums[j]+ nums[k];
+            // 세 숫자의 합이 target이면 result에 저장한다
+            if (sum === target) {
+                result.push([nums[i], nums[j], nums[k]])
+                while(nums[k-1] === nums[k]) k--; // 중복되는 세번째는 스킵한다
+                while(nums[j] === nums[j+1]) j++; // 중복되는 두번째는 스킵한다
+                // 이제서야 진정으로 두번째와 세번째를 이동한다
+                k--; 
+                j++;
+            } else if (sum > target) { // 세 숫자의 합이 target보다 크면 세번째 숫자를 줄인다
+                k--;
+            } else { // 세 숫자의 합이 target보다 작으면 두번째 숫자를 늘린다
+                j++;
+            }
+        }
+    }
+    return result;
+};
