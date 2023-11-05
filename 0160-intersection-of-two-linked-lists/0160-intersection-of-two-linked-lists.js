7143358fa18423c26922a5495cd50ecc1f9b7158
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-05 23:36:43 +0900

    Time: 85 ms (25.54%), Space: 49.6 MB (83.19%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+
+// 5+6 = 6+5 (한번만 교차하면 된다)
+var getIntersectionNode = function(headA, headB) {
+    let pA = headA;
+    let pB = headB;
+    let started = false;
+    let switchCount = 0;
+    
+    // 아직 시작 안 했거나, 시작했는데 둘다 원래자리로 돌아오기 전까지 before return to init position
+    // while(!started || (started && (pA !== headA || pB !== headB))) {
+    while(switchCount < 2) {
+        started = true;
+        // 둘이 만남
+        if (started && pA === pB) {
+            return pA;
+        }
+        if (pA && pA.next) {
+            pA = pA.next;
+        } else {
+            pA = headB;
+            switchCount++;
+        }
+        
+        if (pB && pB.next) {
+            pB = pB.next;
+        } else {
+            pB = headA;
+        }
+    }
+    return null;
+};
