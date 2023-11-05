8f3ef051c5ac106d58a94bbc5ec119f9e1abe3e4
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-05 16:02:34 +0900

    Time: 49 ms (97.12%), Space: 44.3 MB (40.02%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let p = head;
+    
+    while(p && p.next) {
+        let nextP = p.next;
+        
+        if (p.val === nextP.val) {
+            p.next = nextP.next;
+        } else {
+            p = p.next;
+        }
+    }
+    return head;
+};
