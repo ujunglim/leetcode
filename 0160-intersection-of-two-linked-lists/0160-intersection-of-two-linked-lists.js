57a6bcbbfb5f9c783371cee32afc3db9e004f767
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-06 08:40:31 +0900

    Time: 78 ms (53.40%), Space: 50.2 MB (40.14%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,29 +12,54 @@
  * @return {ListNode}
  */
 
-// 5+6 = 6+5 (한번만 교차하면 된다)
+// 내 방법 switchCount
+// // 5+6 = 6+5 (한번만 교차하면 된다)
+// var getIntersectionNode = function(headA, headB) {
+//     let pA = headA;
+//     let pB = headB;
+//     let switchCount = 0;
+    
+//     // 포인터 교환이 2번 미만일때 까지
+//     while(switchCount < 2) {
+//         // 둘이 만남
+//         if (switchCount > 0 && pA === pB) {
+//             return pA;
+//         }
+//         if (pA && pA.next) {
+//             pA = pA.next;
+//         } else {
+//             pA = headB;
+//             switchCount++;
+//         }
+        
+//         if (pB && pB.next) {
+//             pB = pB.next;
+//         } else {
+//             pB = headA;
+//         }
+//     }
+//     return null;
+// };
+
+// 한번 swtich하고 바로 null로 변경
 var getIntersectionNode = function(headA, headB) {
     let pA = headA;
     let pB = headB;
-    let switchCount = 0;
     
-    // 포인터 교환이 2번 미만일때 까지
-    while(switchCount < 2) {
+    while(pA && pB) {
         // 둘이 만남
-        if (switchCount > 0 && pA === pB) {
+        if (pA === pB) {
             return pA;
         }
-        if (pA && pA.next) {
-            pA = pA.next;
-        } else {
+        pA = pA.next;
+        if (!pA) {
             pA = headB;
-            switchCount++;
+            headB = null;
         }
-        
-        if (pB && pB.next) {
-            pB = pB.next;
-        } else {
+        pB = pB.next;
+        if (!pB) {
             pB = headA;
+            headA = null;
         }
     }
     return null;
