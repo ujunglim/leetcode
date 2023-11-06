e0dbf7be6226cda2999b1e6ca80c0eef1cfef345
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-06 09:29:28 +0900

    Time: 64 ms (74.12%), Space: 47 MB (27.13%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -10,24 +10,41 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function(head, val) {
-    // 노드가 없으면 빈 노드자체를 리턴
-    // if (!head) {
-    //     return head;
-    // }
-    let currP = head;
-    let emptyNode = new ListNode();
-    let prevP = emptyNode;
+// var removeElements = function(head, val) {
+//     // 노드가 없으면 빈 노드자체를 리턴
+//     // if (!head) {
+//     //     return head;
+//     // }
+//     let currP = head;
+//     let emptyNode = new ListNode();
+//     let prevP = emptyNode;
     
-    while(currP) {
-        if (currP.val === val) {
-            currP = currP.next;
+//     while(currP) {
+//         if (currP.val === val) {
+//             currP = currP.next;
+//         } else {
+//             prevP.next = currP;
+//             prevP = currP;
+//             currP = currP.next;
+//         }
+//     }
+//     prevP.next = currP;
+//     return emptyNode.next;
+// };
+// currP를 판단해서 prevP이동
+
+
+// p.next를 판단해서 p를 이동
+var removeElements = function(head, val) {
+    const emptyNode = new ListNode(0, head);
+    let p = emptyNode;
+    
+    while(p.next) {
+        if (p.next.val === val) {
+            p.next = p.next.next;
         } else {
-            prevP.next = currP;
-            prevP = currP;
-            currP = currP.next;
+            p = p.next;
         }
     }
-    prevP.next = currP;
-    return emptyNode.next;
+    return emptyNode.next
 };
