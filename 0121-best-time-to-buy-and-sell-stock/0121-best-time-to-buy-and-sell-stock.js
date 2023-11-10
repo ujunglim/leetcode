c2862371d73940897ba0c9caab830737c7311e0b
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-10 18:02:29 +0900

    Time: 75 ms (41.54%), Space: 52.3 MB (6.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,29 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0];
-    let max = 0;
+    let min = Infinity;
+    let max = -Infinity;
     
-    for(let i = 1; i < prices.length; i++) {
-        min = Math.min(prices[i], min);
-        max = Math.max(max, prices[i]-min);
+    for (const price of prices) {
+        min = Math.min(min, price);
+        max = Math.max(max, price-min);
     }
     return max;
 };
+// 최소 = Min(전의 최소, 현재)
+// 최대 = Max(전의 최대, 현재 - 최소)
+
+
+
+
+
+// var maxProfit = function(prices) {
+//     let min = prices[0];
+//     let max = 0;
+    
+//     for(let i = 1; i < prices.length; i++) {
+//         min = Math.min(prices[i], min);
+//         max = Math.max(max, prices[i]-min);
+//     }
+//     return max;
+// };
