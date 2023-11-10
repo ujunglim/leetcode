a790eb9718a0ae5accdd2513ad09c43f8dedb857
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-10 10:05:57 +0900

    Time: 92 ms (96.07%), Space: 63.7 MB (99.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -9,42 +9,60 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    const arr = [];
-    while(head) {
-        arr.push(head.val);
-        head = head.next;
-    }
-    let leftP = 0;
-    let rightP = arr.length-1;
-    
-    while(leftP < rightP) {
-        if (arr[leftP] !== arr[rightP]) {
-            return false
-        }
-        leftP++;
-        rightP--;
-    }
-    return true;
-};
-
 // var isPalindrome = function(head) {
 //     const arr = [];
 //     while(head) {
 //         arr.push(head.val);
 //         head = head.next;
 //     }
+//     let leftP = 0;
+//     let rightP = arr.length-1;
     
-//     let i = 0; 
-//     let j = arr.length-1;
-    
-//     while(i < j) {
-//         if (arr[i] !== arr[j]) {
-//             return false;
+//     while(leftP < rightP) {
+//         if (arr[leftP] !== arr[rightP]) {
+//             return false
 //         }
-//         i++;
-//         j--;
+//         leftP++;
+//         rightP--;
 //     }
 //     return true;
 // };
 
+var isPalindrome = function(head) {
+    let slowP = head;
+    let fastP = head;
+    let reverseHead = null;
+    let slowNext;
+    
+    // [1]
+    if (!head.next) {
+        return true;
+    }
+    
+    while(fastP && fastP.next) {
+        // console.log(slowP, fastP)
+        fastP = fastP.next.next; // 먼저
+        // reverse
+        slowNext = slowP.next;
+        slowP.next = reverseHead;
+        reverseHead = slowP;
+        slowP = slowNext;
+        // console.log('====', slowP, fastP)
+    }
+    let rightP = slowP; //
+    if (fastP && !fastP.next) {
+        rightP = slowP.next;
+    }
+    
+    while(rightP) {
+        if(reverseHead.val !== rightP.val) {
+            return false;
+        }
+        reverseHead = reverseHead.next;
+        rightP = rightP.next;
+    }
+    return true;
+};
+
+
+// slow fast (반절 이동할때까지 reverse)
