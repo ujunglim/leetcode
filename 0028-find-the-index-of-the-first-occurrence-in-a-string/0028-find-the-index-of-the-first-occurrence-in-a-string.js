d1e39f3d72506fb626527218c2e73c08955b5a81
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-20 18:21:34 +0900

    Time: 48 ms (75.62%), Space: 41.5 MB (90.58%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    for (let i = 0; i < haystack.length; i++) {
+    for (let i = 0; i < haystack.length; ++i) {
         if (haystack[i] === needle[0]) {
-            let sameCount = 1;
-            for (let j = 1; j < needle.length; j++) {
-                if (haystack[j+i] !== needle[j]) {
+            let matchCount = 1;
+            for (let j = 1; j < needle.length; ++j) {
+                if (haystack[i+j] !== needle[j]) {
                     break;
                 } else {
-                    sameCount++;
+                    matchCount++;
                 }
             }
-            if (sameCount === needle.length) {
+            if (matchCount === needle.length) {
                 return i;
             }
         }
-    }  
+    }
     return -1;
 };
