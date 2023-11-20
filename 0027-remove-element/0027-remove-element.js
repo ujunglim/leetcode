2ef1f37939b15e818abedcb926546a7d1e988f5d
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-20 11:09:25 +0900

    Time: 59 ms (18.57%), Space: 41.4 MB (92.12%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -3,24 +3,24 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    let left = 0;
-    let right = nums.length-1;
-    
-    while(left <= right) {
-        // 교체당할 곳과 교체할 곳을 모두 찾으면 교체한다
-        if (nums[left] === val && nums[right] !== val) {
-            nums[left] = nums[right];
-            left++; // 교체 후 이동
-            right--;
-        } else {
-            if (nums[left] !== val) {
-                left++;
-            }
-            if (nums[right] === val) {
-                right--;
-            }
-        }
+var removeElement = function (nums, val) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) { /// <=
+    // 교체당할 곳과 교체할 곳을 모두 찾으면 교체한다
+    if (nums[left] === val && nums[right] !== val) {
+      nums[left] = nums[right];
+      left++; // 교체 후 이동
+      right--;
+    } else {
+      if (nums[left] !== val) {
+        left++;
+      }
+      if (nums[right] === val) {
+        right--;
+      }
     }
-    return right+1; 
+  }
+  return right + 1;  // 
 };
