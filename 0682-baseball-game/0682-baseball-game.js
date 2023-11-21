9ae831d1463929043023d3f2c42e97c20e83a5dd
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-21 12:08:30 +0900

    Time: 242 ms (5.09%), Space: 52 MB (5.09%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    const stack = [];
+    for (const op of operations) {
+        console.log(stack)
+        const element = Number(op);
+        // 숫자일때
+        if (!isNaN(element)) {
+            stack.push(element);
+        }
+        else if (op === 'C') {
+            stack.pop();
+        }
+        else if (op === 'D' && stack.length >= 1) {
+            stack.push(stack[stack.length-1]*2)
+        }
+        else if (op === '+' && stack.length >= 2) {
+            stack.push(stack[stack.length-2] + stack[stack.length-1]);
+        }
+    }
+    return stack.reduce((acc, curr) => acc + curr, 0);
+};
