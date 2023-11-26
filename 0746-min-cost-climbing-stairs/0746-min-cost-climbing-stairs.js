d7b43eb88e092ecd958bf96eb103580586cafd30
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-26 22:44:52 +0900

    Time: 60 ms (33.99%), Space: 43.9 MB (37.27%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -2,14 +2,34 @@
  * @param {number[]} cost
  * @return {number}
  */
+// iteration
+// var minCostClimbingStairs = function(cost) {
+//     if (cost.length === 2) {
+//         return Math.min(cost[0], cost[1]);
+//     }
+//     const dp = [cost[0], cost[1]];
+//     for(let i = 2; i < cost.length; ++i) {
+//         dp[i] = Math.min(dp[i-2], dp[i-1]) + cost[i];
+//     }
+//     return Math.min(dp[dp.length-2], dp[dp.length-1]);
+// };
+
+// recursive
 var minCostClimbingStairs = function(cost) {
-    if (cost.length === 2) {
-        return Math.min(cost[0], cost[1]);
-    }
     const dp = [cost[0], cost[1]];
+    const len = cost.length;
     
-    for(let i = 2; i < cost.length; ++i) {
-        dp[i] = Math.min(dp[i-2], dp[i-1]) + cost[i];
+    function getMinCost(i) {
+        if (i < 0) {
+            return 0;
+        }
+        if(dp[i]) {
+            return dp[i];
+        } else {
+            dp[i] = Math.min(getMinCost(i-2), getMinCost(i-1)) + (cost[i] || 0);
+            return dp[i];
+        }
     }
-    return Math.min(dp[dp.length-2], dp[dp.length-1]);
+    return getMinCost(len);
+    
 };
