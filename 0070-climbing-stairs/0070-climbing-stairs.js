7ed6544d1a20944caa13bdbf165918e330db8f0e
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-26 14:51:53 +0900

    Time: 54 ms (33.90%), Space: 41.5 MB (72.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,12 +2,24 @@
  * @param {number} n
  * @return {number}
  */
+// recursive
+// const memory = [0, 1, 2];
+// var climbStairs = function(n) {
+//     if (!memory[n]) {
+//         memory[n] = climbStairs(n-1) + climbStairs(n-2); 
+//     }
+//     return memory[n];
+// };
 
-const memory = [0, 1, 2];
 // iteration
 var climbStairs = function(n) {
-    if (!memory[n]) {
-        memory[n] = climbStairs(n-1) + climbStairs(n-2); 
+    if (n <= 2) {
+        return n;
     }
+    const memory = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        memory[i] = memory[i-1] + memory[i-2];
+    }
+    
     return memory[n];
 };
