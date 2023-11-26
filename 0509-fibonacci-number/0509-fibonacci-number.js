04e11c2362b62a677f957cf3573423286658bcba
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-26 22:55:49 +0900

    Time: 55 ms (58.29%), Space: 41.7 MB (64.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,14 +2,28 @@
  * @param {number} n
  * @return {number}
  */
+// var fib = function(n) {
+//     const dp = [0, 1];
+//     if (dp[n]) {
+//         return dp[n];
+//     }
+//     for (let i = 2; i <= n; ++i) {
+//         dp[i] = dp[i-2] + dp[i-1];
+//     }
+//     return dp[n];
+// };
+
 var fib = function(n) {
     const dp = [0, 1];
-    
-    if (dp[n]) {
-        return dp[n];
+    function recursive(i) {
+        if (i < 0) {
+            return 0;
+        }
+        if (dp[i]) {
+            return dp[i];
+        }
+        dp[i] = recursive(i-2) + recursive(i-1);
+        return dp[i];
     }
-    for (let i = 2; i <= n; ++i) {
-        dp[i] = dp[i-2] + dp[i-1];
-    }
-    return dp[n];
+    return recursive(n);
 };
