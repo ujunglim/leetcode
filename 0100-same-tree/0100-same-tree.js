1545ec7db2952f8aebb7740e417d1bddf52897dd
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-27 12:29:12 +0900

    Time: 56 ms (26.95%), Space: 41.9 MB (84.05%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    function checkIsSame(i, j) {
+        // 둘 다 존재하지 않음
+        if (!i && !j) {
+            return true;
+        }
+        // 둘 중 하나가 존재하지 않음
+        if (i && !j || !i && j) {
+            return false;
+        }
+        // 둘 다 존재하는데 따름
+        if (i.val !== j.val) {
+            return false;
+        }
+        // 같음
+        if (i.val === j.val) {
+            return checkIsSame(i.left, j.left) && checkIsSame(i.right, j.right);
+        }
+    }
+    return checkIsSame(p, q);
+};
