1deb9061acfb67c92148779ecdb720e6e5b3756c
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-27 12:40:54 +0900

    Time: 48 ms (76.27%), Space: 41.9 MB (84.05%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -11,18 +11,39 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+// dfs
+// var isSameTree = function(p, q) {
+//     function checkIsSame(i, j) {
+//         // 둘 다 존재하지 않음
+//         if (!i && !j) {
+//             return true;
+//         }
+//         // 둘 중 하나가 존재하지 않거나 둘 다 존재하는데 값이 다름
+//         if (i && !j || !i && j || i.val !== j.val) {
+//             return false;
+//         }
+//         // 같음
+//         return checkIsSame(i.left, j.left) && checkIsSame(i.right, j.right);
+//     }
+//     return checkIsSame(p, q);
+// };
+
+// bfs
 var isSameTree = function(p, q) {
-    function checkIsSame(i, j) {
-        // 둘 다 존재하지 않음
-        if (!i && !j) {
-            return true;
-        }
-        // 둘 중 하나가 존재하지 않거나 둘 다 존재하는데 값이 다름
-        if (i && !j || !i && j || i.val !== j.val) {
+    const queue = [p, q];
+    
+    while(queue.length) {
+        const first = queue.shift();
+        const second = queue.shift();
+        
+        if (!first && !second) continue;
+        if (!first || !second || first.val !== second.val) {
             return false;
         }
-        // 같음
-        return checkIsSame(i.left, j.left) && checkIsSame(i.right, j.right);
+        queue.push(first.left);
+        queue.push(second.left);
+        queue.push(first.right);
+        queue.push(second.right);
     }
-    return checkIsSame(p, q);
+    return true;
 };
