6059290e5cf9e4ec09d9e23ed7aafe27b835f0ed
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-29 17:45:25 +0900

    Time: 50 ms (81.31%), Space: 43.8 MB (63.84%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -10,27 +10,56 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function(root) {
-    if (!root) return []
-    const answer = [];
-    const queue = [root];
-    let depth = 0;
+// bfs
+// var zigzagLevelOrder = function(root) {
+//     if (!root) return [];
+//     const answer = [];
+//     const queue = [root];
+//     let depth = 0;
     
-    while(queue.length) {
-        const levelSize = queue.length;
-        const level = [];
-        for (let i = 0; i < levelSize; ++i) {
-            const currNode = queue.shift();
-            if (depth % 2 === 0) {
-                level.push(currNode.val);
-            } else {
-                level.unshift(currNode.val);
-            }
-            currNode.left && queue.push(currNode.left);
-            currNode.right && queue.push(currNode.right);
+//     while(queue.length) {
+//         const levelSize = queue.length;
+//         const level = [];
+//         for (let i = 0; i < levelSize; ++i) {
+//             const currNode = queue.shift();
+//             if (depth % 2 === 0) {
+//                 level.push(currNode.val);
+//             } else {
+//                 level.unshift(currNode.val);
+//             }
+//             currNode.left && queue.push(currNode.left);
+//             currNode.right && queue.push(currNode.right);
+//         }
+//         answer.push(level);
+//         depth++;
+//     }
+//     return answer;
+// };
+
+
+// dfs
+var zigzagLevelOrder = function(root) {
+    if (!root) return [];
+    const answer = [];
+    
+    function dfs(currNode, depth) {
+        if (!currNode) {
+            return;
         }
-        answer.push(level);
-        depth++;
+        if (!answer[depth]) {
+            answer[depth] = [];
+        }
+        
+        if (depth % 2 === 0) {
+            answer[depth].push(currNode.val);
+        } else {
+            answer[depth].unshift(currNode.val);
+        }
+        
+        dfs(currNode.left, depth+1);
+        dfs(currNode.right, depth+1);
     }
+    
+    dfs(root, 0);
     return answer;
 };
