e715057f535610c36bf0c0b6cbe34fe0430b435b
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-29 12:33:52 +0900

    Time: 60 ms (55.95%), Space: 44.1 MB (87.50%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+    let answer = 0;
+    
+    function dfs(currNode, accStr) {
+        if (!currNode) {
+            return;
+        }
+        if (!currNode.left && !currNode.right) {
+            answer += Number(parseInt(accStr+currNode.val, 2));
+            return;
+        }
+        dfs(currNode.left, accStr + currNode.val);
+        dfs(currNode.right, accStr + currNode.val);
+    }
+    
+    dfs(root, '');
+    return answer;
+};
