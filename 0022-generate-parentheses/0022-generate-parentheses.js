4bcc62d46353cb2a1ecf56f8ea3c260a20305a68
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-01 16:35:50 +0900

    Time: 43 ms (95.82%), Space: 44.2 MB (15.15%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -2,48 +2,44 @@
  * @param {number} n
  * @return {string[]}
  */
-// 더 빠르게 )의 갯수가 (보다 많으면 바로 리턴
 var generateParenthesis = function(n) {
-    const answer = [];
-    const len = n*2;
-    
-    function bt(accArr, openCount, closeCount) {
-        if (openCount < closeCount) {
-            return;
-        }
-        const count = accArr.length;
-        if (count === len) {
-            if (isValid(accArr)) {
-                answer.push(accArr.join(''));
-            }
-            return;
-        }
-        bt([...accArr, '('], openCount+1, closeCount);
-        bt([...accArr, ')'], openCount, closeCount+1);
+  const output = [];
+  /**
+   * Examples 
+   * n = 1
+   *  ()
+   *
+   *  n = 2
+   *  (())
+   *  ()()
+   *
+   *  n = 3
+   *  ((()))
+   *  (()())
+   *  (())()
+   *  ()(())
+   *  ()()()
+   */
+  const dfs = (str, open, close) => {
+    // Close parentheses can not be more than open parentheses at any 
+    // given time to stay valid.
+    if (open > close) {
+      return;
     }
-    
-    function isValid(arr) {
-        const stack = [];
-        
-        for (let i = 0; i < arr.length; ++i) {
-            if (!stack.length) {
-                stack.push(arr[i]);
-                continue;
-            }
-            
-            if (arr[i] === ')') {
-                if (stack[stack.length-1] === '(') {
-                    stack.pop();
-                    continue;
-                } 
-            } else {
-                stack.push('(')
-            }
-        }
-        return !stack.length;
+    // Base case. We now have n pairs of parentheses
+    if (open === 0 && close === 0) {
+      output.push(str);
+      return;
     }
-    
-    bt([], 0, 0); // str, openCount, closeCount
-    return answer;
+    // Insert open parenthsis and search for the next valid insertion.
+    if (open > 0) {
+      dfs(`${str}(`, open - 1, close);
+    }
+    // Insert close parenthsis and search for the next valid insertion.
+    if (close > 0) {
+      dfs(`${str})`, open, close - 1);
+    }
+  };
+  dfs('', n, n);
+  return output;
 };
-    
