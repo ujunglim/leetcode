f6d6553cc62343191aa7c44efddce973fff0c253
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-08 11:02:15 +0900

    Time: 67 ms (33.05%), Space: 45.4 MB (63.84%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -1,16 +1,29 @@
+// /**
+//  * @param {string} num
+//  * @return {string}
+//  */
+// var largestOddNumber = function(num) {
+//     for (let i = num.length-1; i >= 0; --i) {
+//         if (Number(num[i]) % 2 === 1) {
+//             return num.substring(0, i+1);
+//         }
+//     }
+//     return '';
+// };
+
 /**
  * @param {string} num
  * @return {string}
  */
 var largestOddNumber = function(num) {
-    // const numArr = num.split('').map(e => Number(e));
-    
-    for (let i = num.length-1; i >= 0; --i) {
-        if (Number(num[i]) % 2 === 1) {
-            return num.substring(0, i+1);
-            // return num.slice(0, i+1).join('');
-        }
+    if (parseInt(num.slice(-1)) % 2 === 1) return num;
+    let i = num.length - 1;
+    while (i >= 0) {
+        const n = parseInt(num[i]);
+        if (n % 2 === 1) return num.slice(0, i + 1);
+        i--;
     }
-    return '';
+    return "";
 };
 
+    
